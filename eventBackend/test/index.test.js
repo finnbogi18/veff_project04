8a293313b151b67b534bc7a28c1d8d4443c86534
Konn1nn <54936310+Konn1nn@users.noreply.github.com>
2020-03-28 16:44:47 +0000

    what I have done :)

--- eventBackend/test/index.test.js
+++ eventBackend/test/index.test.js
@@ -49,4 +49,60 @@ describe('Endpoint tests', () => {
         console.log("Our booking has id " + bookingId);
         chai.expect(1).to.equal(1);
     });
+    
+    it("GET /events", function (done) {
+        chai.request('http://localhost:3000/api/v1').get('/events').end( (err, res) => {
+            chai.expect(res).to.have.status(200);
+            chai.expect(res).to.be.json;
+            chai.expect(res.body).to.be.a('object');
+            chai.expect(res.body).to.be.an('array');
+            chai.expect(Object.keys(res.body).length).to.be.eql(1);
+            done();
+        });
+    });
+
+    it("GET /events/:eventId", function (done) {
+        chai.request('http://localhost:3000/api/v1').get('/events/'+ eventId).end( (err, res) => {
+            chai.expect(res).to.have.status(200);
+            chai.expect(res).to.be.json;
+            chai.expect(res.body).to.be.a('object');
+            chai.expect(Object.keys(res.body).length).to.be.eql(4);
+            chai.expect()
+            done();
+        });
+    });
+
+    it("POST /events", function (done) {
+        chai.request('http://localhost:3000/api/v1')
+            .post('/events')
+            .set('Content-type', 'application/json')
+            .send({	'name': 'Arshatid HR', 'capacity': 3, 'startDate': '158458972000', 'endDate': '1584585972000', 'description': 'This was cancelled :/', 'location': 'Vodafonehollin'})
+            .end( (err, res) => {
+            chai.expect(res).to.have.status(201);
+            chai.expect(res).to.be.json;
+            chai.expect(res.body).to.be.a('object');
+            chai.expect(res.body).to.have.property('name').eql('Arshatid HR');
+            chai.expect(res.body).to.have.property('capacity').eql(3);
+            chai.expect(res.body).to.have.property('description').eql('This was cancelled :/');
+            chai.expect(res.body).to.have.property('location').eql('Vodafonehollin');
+            chai.expect(res.body).to.have.property('_id');
+            chai.expect(Object.keys(res.body).length).to.be.eql(7);
+            done();
+        });
+    });
+
+    it("POST /users failure", function (done) {
+        chai.request('http://localhost:3000/api/v1')
+            .post('/users')
+            .set('Content-type', 'application/json')
+            .send({'age':'10'})
+            .end( (err, res) => {
+            chai.expect(res).to.have.status(400);
+            chai.expect(res).to.be.json;
+            chai.expect(res.body).to.be.a('object');
+            chai.expect(res.body).to.have.property('message').eql('No username defined.');
+            chai.expect(Object.keys(res.body).length).to.be.eql(1);
+            done();
+        });
+    });
 });
